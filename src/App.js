0113026faf0d67f35ae78bb56ac396ff2c4742f9
Chrisfaherty <chrismactom@gmail.com>
2023-05-12 09:19:24 +0000

    Bug, Main class in App.modules.css had a small M so the css was not being generated on the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import NavBar from "./components/NavBar";
-import styles from './App.module.css';
-import Container from 'react-bootstrap/Container';
-import { Route, Switch } from 'react-router-dom';
+import styles from "./App.module.css";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
@@ -9,12 +9,12 @@ function App() {
       <NavBar/>
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={()=> <h1>Home page</h1> }/>
-          <Route exact path="/signin" render={()=> <h1>Sign in</h1> }/>
-          <Route exact path="/signup" render={()=> <h1>Sign up</h1> }/>
+          <Route exact path="/" render={()=> <h1>Home page</h1> } />
+          <Route exact path="/signin" render={()=> <h1>Sign in</h1> } />
+          <Route exact path="/signup" render={()=> <h1>Sign up</h1>  }/>
+          <Route render={()=><p>Page not found!</p>}/>
         </Switch>
-        
-        
+
       </Container>
     </div>
   );
