88afbb0030ddd99d03f7aabfa4a6fc72ff8577c6
Chrisfaherty <chrismactom@gmail.com>
2023-05-12 15:23:37 +0000

    Bug: axiosDefaults had the front end deployed url instead of the backend

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://ci-nft-social-hub.herokuapp.com/';
+axios.defaults.baseURL = 'https://ci-nft-social-hub-api.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
