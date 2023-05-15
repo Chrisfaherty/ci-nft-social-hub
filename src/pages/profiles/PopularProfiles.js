3c5a65b54afcd4f792e294192aa942cb14d3da0c
Chrisfaherty <chrismactom@gmail.com>
2023-05-15 11:32:17 +0000

    Define the profile data object

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import appStyles from '../../App.module.css'
 import { Container } from 'react-bootstrap'
 
 const PopularProfiles = () => {
+    const [profileData, setProfileData] = useState({
+        // we will use the pageProfile later!
+        pageProfile: { results: [] },
+        popularProfiles: { results: [] },
+    })
   return (
     <Container className={appStyles.Content}>
         <p>Most followed profiles.</p>
