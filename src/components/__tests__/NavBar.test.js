76925cc83c858c3e44be776b6bf069a2e719a03c
Chrisfaherty <chrismactom@gmail.com>
2023-05-16 07:00:47 +0000

    Set up a test to see if a user is logged in by checking for a profile avatar

--- src/components/__tests__/NavBar.test.js
+++ src/components/__tests__/NavBar.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import {BrowserRouter as Router} from 'react-router-dom'
 import NavBar from '../NavBar'
+import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
 
 
 test('renders NavBar', () => {
@@ -12,4 +13,17 @@ test('renders NavBar', () => {
     //screen.debug();
     const signInLink = screen.getByRole('link', {name: 'Sign in'})
     expect(signInLink).toBeInTheDocument()
+});
+
+
+test('renders link to the user profile for a logged in user', async () => {
+    render(
+    <Router>
+        <CurrentUserProvider>
+            <NavBar />
+        </CurrentUserProvider>
+    </Router>
+    );
+    const profileAvatar = await screen.findByText('Profile');
+    expect(profileAvatar).toBeInTheDocument();
 });
