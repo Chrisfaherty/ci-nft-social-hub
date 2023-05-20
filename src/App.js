5ccdf12eaef4f81c2d0dfe99721e4609f076c946
Chrisfaherty <chrismactom@gmail.com>
2023-05-20 18:55:07 +0000

    Add a subscribe route to app.js

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ function App() {
           />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/subscribe" render={() => <SubscribeForm />}/>
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
