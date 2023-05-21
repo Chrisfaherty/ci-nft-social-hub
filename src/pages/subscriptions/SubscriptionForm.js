187eb198afffeb44ef9815097f94a9fbdae11f1d
Chrisfaherty <chrismactom@gmail.com>
2023-05-21 08:28:28 +0000

    Create the basic structure for the subscription form

--- src/pages/subscriptions/SubscriptionForm.js
+++ src/pages/subscriptions/SubscriptionForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
@@ -8,7 +8,6 @@ import appStyles from "../../App.module.css";
 import {
   Form,
   Button,
-  Image,
   Col,
   Row,
   Container,
@@ -17,24 +16,23 @@ import {
 import axios from "axios";
 import { useRedirect } from "../../hooks/useRedirect";
 
-const SignUpForm = () => {
+const SubscriptionForm = () => {
 
   useRedirect('loggedIn')
 
-  const [signUpData, setSignUpData] = useState({
-    username: "",
-    password1: "",
-    password2: "",
+  const [subscriptionData, setSubscriptionData] = useState({
+    fullname: "",
+    email: "",
   });
-  const { username, password1, password2 } = signUpData;
+  const { fullname, email} = subscriptionData;
 
   const [errors, setErrors] = useState({});
 
   const history = useHistory();
 
   const handleChange = (event) => {
-    setSignUpData({
-      ...signUpData,
+    setSubscriptionData({
+      ...subscriptionData,
       [event.target.name]: event.target.value,
     });
   };
@@ -42,8 +40,8 @@ const SignUpForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("/dj-rest-auth/registration/", signUpData);
-      history.push("/signin");
+      await axios.post("/dj-rest-auth/registration/", subscriptionData);
+      history.push("/");
     } catch (err) {
       setErrors(err.response?.data);
     }
@@ -51,66 +49,50 @@ const SignUpForm = () => {
 
   return (
     <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
+      <Col className="my-auto py-2 p-md-2" md={8}>
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign up</h1>
+          <h1 className={styles.Header}>subscribe to stay informed</h1>
 
           <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="username">
-              <Form.Label className="d-none">username</Form.Label>
+            <Form.Group controlId="fullname">
+              <Form.Label className="d-none">fullname</Form.Label>
               <Form.Control
                 className={styles.Input}
                 type="text"
-                placeholder="Username"
-                name="username"
-                value={username}
+                placeholder="Fullname"
+                name="fullname"
+                value={fullname}
                 onChange={handleChange}
               />
             </Form.Group>
-            {errors.username?.map((message, idx) => (
+            {errors.fullname?.map((message, idx) => (
               <Alert variant="warning" key={idx}>
                 {message}
               </Alert>
             ))}
 
-            <Form.Group controlId="password1">
-              <Form.Label className="d-none">Password</Form.Label>
+            <Form.Group controlId="email">
+              <Form.Label className="d-none">Email Address</Form.Label>
               <Form.Control
                 className={styles.Input}
-                type="password"
-                placeholder="Password"
-                name="password1"
-                value={password1}
+                type="email"
+                placeholder="Email Address"
+                name="email"
+                value={email}
                 onChange={handleChange}
               />
             </Form.Group>
-            {errors.password1?.map((message, idx) => (
+            {errors.email?.map((message, idx) => (
               <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
 
-            <Form.Group controlId="password2">
-              <Form.Label className="d-none">Confirm password</Form.Label>
-              <Form.Control
-                className={styles.Input}
-                type="password"
-                placeholder="Confirm password"
-                name="password2"
-                value={password2}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.password2?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
             <Button
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
               type="submit"
             >
-              Sign up
+              Subscribe Now!
             </Button>
             {errors.non_field_errors?.map((message, idx) => (
               <Alert key={idx} variant="warning" className="mt-3">
@@ -120,23 +102,9 @@ const SignUpForm = () => {
           </Form>
         </Container>
 
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signin">
-            Already have an account? <span>Sign in</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"}
-        />
       </Col>
     </Row>
   );
 };
 
-export default SignUpForm;
+export default SubscriptionForm;
