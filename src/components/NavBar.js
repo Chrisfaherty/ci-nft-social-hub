2f284fa6f1ccbbda88af355aa5f03d2cd6be37ca
Chrisfaherty <chrismactom@gmail.com>
2023-05-26 20:26:35 +0000

    Add the new logo and change the height

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -105,7 +105,7 @@ const NavBar = () => {
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
-            <img src={logo} alt="logo" height="45" />
+            <img src={logo} alt="logo" height="50" />
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
